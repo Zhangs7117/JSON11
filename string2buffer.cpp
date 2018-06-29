e8470e1a1d4d9365eefc81bc7c7fa4435b2aa235
Zhang Shuo <SENSETIME\zhangshuo@shst18045082.domain.sensetime.com>
2018-06-29 19:49:35 +0800

    solve the memory question

--- string2buffer.cpp
+++ string2buffer.cpp
@@ -2,12 +2,74 @@
 using namespace json11;
 using std::string;
 
-int string2buffer (const std::string  &json, char* buffer, void* fmt){
+
+
+int string2buffer (const std::string  &json_in,const std::string  &json_out, char*& buffer, int *& locs){
+    
     string err;
-    const auto jsonparse = Json::parse(json, err);
-    strcpy(buffer,jsonparse["name"].string_value().c_str());
-    strcpy(buffer+25,jsonparse["id_number"].string_value().c_str());
-    // std::cout<<err<<std::endl;
-    if (err[0]=='\0') return 1;
-    else return -1;
+    const auto jsonparse_in = Json::parse(json_in, err);
+    std::map<std::string, Json> json_obj = jsonparse_in.object_items();
+    std::map<std::string, Json>::iterator iter;
+
+    int key_num = json_obj.size();  
+    int loc = 0;
+    int buffer_len = 0;
+    
+    for (iter = json_obj.begin();iter != json_obj.end(); iter++ ){
+        buffer_len += iter->second.number_value();
+    }
+
+    buffer = new char[buffer_len];
+    locs = new int [key_num];
+
+    const auto jsonparse_out = Json::parse(json_out, err);
+    int i=0;
+    for (iter = json_obj.begin();iter != json_obj.end(); iter++ ){
+        strcpy((buffer+loc),jsonparse_out[iter->first].string_value().c_str());
+        locs[i] = loc;
+        loc = loc + iter->second.number_value();
+        i++;
+    }
+
+    return key_num;
+}
+
+/////////////////////////////////////////////////////////////////
+//when u want to divide the function
+/////////////////////////////////////////////////////////////////
+// int getfmt (const std::string  &json, struct jsonfmt * fmt){
+//     string err;
+//     const auto jsonparse = Json::parse(json, err);
+    
+//     std::map<std::string, Json> json_obj = jsonparse.object_items();
+//     std::map<std::string, Json>::iterator iter;
+    
+//     fmt->key_num = json_obj.size();
+//     fmt->key_len = new int [fmt->key_num];
+//     fmt->key_val = new string [fmt->key_num];
+//     int loc = 0;
+//     for (iter = json_obj.begin();iter != json_obj.end(); iter++ ){
+//         fmt->key_val[loc] = iter->first;
+//         fmt->key_len[loc] = iter->second.number_value();
+//         std::cout<<iter->first<<" : "<<iter->second.number_value()<<std::endl;
+//         loc++;
+//     }
+    
+//     if (err[0]=='\0') return 1;
+//     else return -1;
+// }
+
+
+string readFileIntoString(const char * filename)
+{
+std::ifstream ifile(filename);
+
+//read in the stream
+std::ostringstream buf;
+char ch;
+while(buf&&ifile.get(ch))
+buf.put(ch);
+
+//return the str
+return buf.str();
 }
--- string2buffer.hpp
+++ string2buffer.hpp
@@ -4,6 +4,19 @@
 #include <cstring>
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include "json11.hpp"
 
-int string2buffer (const std::string  &json, char* buffer, void* fmt);
+// struct jsonfmt {
+//     std::string *key_val;
+//     int *key_len;
+//     int key_num;
+// };
+
+
+
+int string2buffer (const std::string  &json_in, const std::string  &json_out, char*& buffer, int*& len);
+
+// int getfmt (const std::string  &json, struct jsonfmt * fmt);
+
+std::string readFileIntoString(const char * filename);
